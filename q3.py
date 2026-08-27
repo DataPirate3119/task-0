ac2f0b2a9fce5a2d30240bf4d1e97b27a00e82ce
DataPirate3119 <104068595+DataPirate3119@users.noreply.github.com>
2026-08-27 19:28:07 +0530

    Update q3.py
    Complete Prime Numbers using for-else done in a single function call

--- q3.py
+++ q3.py
@@ -1,12 +1,18 @@
 def is_prime(n):
+    '''
     for i in range(2,n):
         if n%i==0:
             return False
     else:
         return True
+    '''
+    for j in range(2,n+1):
+        for k in range(2,j):
+            if j%k==0:
+                break
+        else:
+            print(j, end=" ")
+    print()
         
 n=int(input())
-for j in range(2,n+1):
-    if is_prime(j)==True:
-        print(j, end=" ")
-print()
+is_prime(n)
